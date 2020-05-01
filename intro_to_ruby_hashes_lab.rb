2ea2c543b78f922b2e65b1668871d747c11aabbf
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-05-01 21:30:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,21 +20,17 @@ def monopoly_with_second_tier
     railroads: {
       pieces: 4,
       names: {
-
+        
+      }
+      :rent_in_dollars => {
+        
       }
     }
   }
 end
 
 def monopoly_with_third_tier
-  monopoly = {
-    :railroads =>
-      {
-        :pieces => 4,
-        :names => {},
-        :rent_in_dollars => {}
-    }
-  }
+
 end
 
 def monopoly_with_fourth_tier
