610097716830c51250e6527a95a9a895ada5279c
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-05-01 21:33:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,7 +36,9 @@ def monopoly_with_third_tier
 
       },
       :rent_in_dollars => {
-        :one_piece_owned => 25
+        :one_piece_owned => 25,
+        :two_pieces_owned => 50,
+        
       }
     }
   }
